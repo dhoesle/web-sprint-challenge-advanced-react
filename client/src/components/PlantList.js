0411d88bdca17973f90d141e99d981c7893fbd96
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-10 09:25:08 -0600

    completed displaying plant list

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,16 +3,28 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  
+  state = {
+    plants: []
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/plants')
+      .then(res => {
+      console.log("PlantList -> componentDidMount -> res", res.data.plantsData)
+        this.setState({
+          plants: res.data.plantsData
+        })
+      })
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
-
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
