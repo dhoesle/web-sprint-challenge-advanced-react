3ca9cc150927a791827125ca1fab19bb2c32fbad
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-10 09:33:58 -0600

    completed checkout form hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from './useLocalStorage'
+
+export const useForm = (key, initalValues) => {
+    const [values, setValues] = useLocalStorage(key, initalValues)
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return[values, handleChanges]
+}
